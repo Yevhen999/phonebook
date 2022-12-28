2ee7d7c3def174790ea3fc54c4fe9b8d2a09f6d1
Yevhen <leshchenkoyevhen@gmail.com>
2022-12-28 18:59:42 -0500

    add createAction and createReducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { createAction, createReducer } from '@reduxjs/toolkit';
+
+const filters = createAction('myCounter/filters');
+console.log(filters.toString());
+
+const myContacts = createReducer([], {});
+
+const myFilter = createReducer('', {});
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    contacts: myContacts,
+    filter: myFilter,
+  },
 });
