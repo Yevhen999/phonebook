dd0b4a49a09df87bd714a614ec7f5f5976e7f864
Yevhen <leshchenkoyevhen@gmail.com>
2023-01-03 18:42:11 -0500

    add alert to addContact

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,40 +1,44 @@
 import { configureStore, nanoid } from '@reduxjs/toolkit';
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = {
+const initialStateContacts = {
   contacts: [
     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
   ],
+};
+
+const initialStateFilter = {
   filter: '',
 };
 
 const filterSlice = createSlice({
   name: 'filter',
-  initialState: initialState,
+  initialState: initialStateFilter,
   reducers: {
     changeFilter: {
       reducer(state, action) {
         state.filter = action.payload;
       },
     },
-    // getVisibleContacts(state, action) {
-    //   const normalizedFilter = state.filter.toLowerCase();
-    //   return state.contacts.filter(contact =>
-    //     contact.name.toLowerCase().includes(normalizedFilter)
-    //   );
-    // },
   },
 });
 
 const contactsSlice = createSlice({
   name: 'contacts',
-  initialState: initialState,
+  initialState: initialStateContacts,
   reducers: {
     addContact: {
       reducer(state, action) {
+        const { name } = action.payload;
+        for (const contact of state.contacts) {
+          if (contact.name.toLowerCase() === name.toLowerCase()) {
+            alert(`${name} is already in contacts`);
+            return;
+          }
+        }
         state.contacts.push(action.payload);
       },
       prepare(contact) {
