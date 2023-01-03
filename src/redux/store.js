f9ea02ff632f4524a44ce46e75da498019d39f35
Yevhen <leshchenkoyevhen@gmail.com>
2023-01-02 20:21:18 -0500

    add reducers

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { createAction, createReducer } from '@reduxjs/toolkit';
 
-const filters = createAction('myCounter/filters');
-console.log(filters.toString());
+const initialContacts = [];
 
-const myContacts = createReducer([], {});
-
-const myFilter = createReducer('', {});
+export const contactsReducer = (state = initialContacts, action) => {
+  // code
+};
 
 export const store = configureStore({
   reducer: {
-    contacts: myContacts,
-    filter: myFilter,
+    contacts: contactsReducer,
   },
 });
