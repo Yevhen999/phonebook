7a19b104ab8a0d13e4a3f08ccfbaeb9baa9524a7
Yevhen <leshchenkoyevhen@gmail.com>
2023-01-03 19:57:29 -0500

    save contacts to localeStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,3 @@
-// import { useState } from 'react';
 import ContactsList from './Contacts/ContactsList';
 import { FormData } from './Contacts/FormData';
 import { Filter } from './Contacts/Filter';
@@ -17,19 +16,6 @@ export const App = () => {
     );
   };
   const visibleContacts = getVisibleContacts();
-
-  // const [contacts, setContacts] = useState(
-  //   JSON.parse(localStorage.getItem('contacts')) ?? [
-  //     { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  //     { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  //     { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  //     { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  //   ]
-  // );
-
-  // useEffect(() => {
-  //   localStorage.setItem('contacts', JSON.stringify(contacts));
-  // }, [contacts]);
 
   return (
     <div
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,16 @@
 import { configureStore, nanoid } from '@reduxjs/toolkit';
 import { createSlice } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 const initialStateContacts = {
   contacts: [
@@ -59,12 +70,30 @@ const contactsSlice = createSlice({
   },
 });
 
+const persistConfig = {
+  key: 'contacts',
+  storage,
+};
+
+const persistedContactsReducer = persistReducer(
+  persistConfig,
+  contactsSlice.reducer
+);
+
 export const { addContact, deleteContact } = contactsSlice.actions;
 export const { changeFilter } = filterSlice.actions;
 
 export const store = configureStore({
   reducer: {
-    contacts: contactsSlice.reducer,
+    contacts: persistedContactsReducer,
     filter: filterSlice.reducer,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
