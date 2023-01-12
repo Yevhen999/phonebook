17d5765a5e65d07911036498de282d031e598efe
Yevhen <leshchenkoyevhen@gmail.com>
2023-01-11 19:15:44 -0500

    add LoginForm and AuthNav

---
+++ src/components/LoginForm/LoginForm.js
@@ -0,0 +1,33 @@
+import { useDispatch } from 'react-redux';
+import { logIn } from 'redux/auth/operations';
+import css from './LoginForm.module.css';
+
+export const LoginForm = () => {
+  const dispatch = useDispatch();
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    dispatch(
+      logIn({
+        email: form.elements.email.value,
+        password: form.elements.password.value,
+      })
+    );
+    form.reset();
+  };
+
+  return (
+    <form className={css.form} onSubmit={handleSubmit} autoComplete="off">
+      <label className={css.label}>
+        Email
+        <input type="email" name="email" />
+      </label>
+      <label className={css.label}>
+        Password
+        <input type="password" name="password" />
+      </label>
+      <button type="submit">Log In</button>
+    </form>
+  );
+};
