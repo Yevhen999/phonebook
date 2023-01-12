fc6a84909741466d378903a6915d77d8cc751fe9
Yevhen <leshchenkoyevhen@gmail.com>
2023-01-11 19:03:39 -0500

    add Home

---
+++ src/pages/Home.js
@@ -0,0 +1,28 @@
+const styles = {
+  container: {
+    minHeight: 'calc(100vh - 50px)',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontWeight: 500,
+    fontSize: 48,
+    textAlign: 'center',
+  },
+};
+
+const Home = () => {
+  return (
+    <div style={styles.container}>
+      <h1 style={styles.title}>
+        Phonebook manager welcome page{' '}
+        <span role="img" aria-label="Greeting icon">
+          💁‍♀️
+        </span>
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
