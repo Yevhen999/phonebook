e9f70c4c9a0dbfe4a6943b90e49432d789141d2d
Yevhen <leshchenkoyevhen@gmail.com>
2023-01-23 19:46:10 -0500

    add styles to IconButton

--- src/components/AuthNav/AuthNav.js
+++ src/components/AuthNav/AuthNav.js
@@ -1,26 +1,26 @@
 import { Box, IconButton, Typography } from '@mui/material';
-import { NavLink } from 'react-router-dom';
+import { AuthLink } from './AuthNav.styled';
 
 export const AuthNav = () => {
   return (
     <Box 
     sx={{
-                  typography: 'body1',
+        typography: 'body1',
       '& > :not(style) + :not(style)': {
         ml: 2,
       },    
     }}
     >
-    <NavLink style={{ textDecoration: "none" }} to="/register">
-      <IconButton sx={{color: 'ButtonFace'}}>
+    <AuthLink to="/register">
+      <IconButton  sx={{color: 'ButtonFace',"&:hover": { color: "black" }}}>
     <Typography >Register</Typography>
     </IconButton>
-      </NavLink>
-            <NavLink style={{ textDecoration: "none" }} to="/login">
+      </AuthLink>
+            <AuthLink to="/login">
             <IconButton sx={{color: 'ButtonFace'}}>
     <Typography>Log in</Typography>
   </IconButton>
-      </NavLink>
+      </AuthLink>
     </Box>
   );
 };
---
+++ src/components/AuthNav/AuthNav.styled.js
@@ -0,0 +1,6 @@
+import styled from "@emotion/styled";
+import { NavLink } from "react-router-dom";
+
+export const AuthLink = styled(NavLink)`
+text-decoration: none;
+`
