73c91d39b01e749116b4070f773512b5801f9c20
Yevhen <leshchenkoyevhen@gmail.com>
2023-01-23 19:22:31 -0500

    edit AuthNav styles

--- src/components/AuthNav/AuthNav.js
+++ src/components/AuthNav/AuthNav.js
@@ -1,15 +1,26 @@
+import { Box, IconButton, Typography } from '@mui/material';
 import { NavLink } from 'react-router-dom';
-import css from './AuthNav.module.css';
 
 export const AuthNav = () => {
   return (
-    <div>
-      <NavLink className={css.link} to="/register">
-        Register
+    <Box 
+    sx={{
+                  typography: 'body1',
+      '& > :not(style) + :not(style)': {
+        ml: 2,
+      },    
+    }}
+    >
+    <NavLink style={{ textDecoration: "none" }} to="/register">
+      <IconButton sx={{color: 'ButtonFace'}}>
+    <Typography >Register</Typography>
+    </IconButton>
       </NavLink>
-      <NavLink className={css.link} to="/login">
-        Log In
+            <NavLink style={{ textDecoration: "none" }} to="/login">
+            <IconButton sx={{color: 'ButtonFace'}}>
+    <Typography>Log in</Typography>
+  </IconButton>
       </NavLink>
-    </div>
+    </Box>
   );
 };
