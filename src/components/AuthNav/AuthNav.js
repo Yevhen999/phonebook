3c982088e22a9a6ce4f68939fe4c4cbce53f9453
Yevhen <leshchenkoyevhen@gmail.com>
2023-01-26 20:04:08 -0500

    refactoring Home

--- src/components/AuthNav/AuthNav.js
+++ src/components/AuthNav/AuthNav.js
@@ -3,23 +3,27 @@ import { NavLink } from 'react-router-dom';
 
 export const AuthNav = () => {
   return (
-    <Box 
-    sx={{
+    <Box
+      sx={{
         typography: 'body1',
-      '& > :not(style) + :not(style)': {
-        ml: 2,
-      },    
-    }}
+        '& > :not(style) + :not(style)': {
+          ml: 2,
+        },
+      }}
     >
-    <NavLink style={{textDecoration: 'none'}} to="/register">
-      <IconButton  sx={{color: 'ButtonFace',"&:hover": { color: "lightblue" }}}>
-    <Typography >Register</Typography>
-    </IconButton>
+      <NavLink style={{ textDecoration: 'none' }} to="/register">
+        <IconButton
+          sx={{ color: 'ButtonFace', '&:hover': { color: 'lightblue' } }}
+        >
+          <Typography>Register</Typography>
+        </IconButton>
       </NavLink>
-            <NavLink style={{textDecoration: 'none'}} to="/login">
-            <IconButton sx={{color: 'ButtonFace',"&:hover": { color: "lightblue" }}}>
-    <Typography>Log in</Typography>
-  </IconButton>
+      <NavLink style={{ textDecoration: 'none' }} to="/login">
+        <IconButton
+          sx={{ color: 'ButtonFace', '&:hover': { color: 'lightblue' } }}
+        >
+          <Typography>Log in</Typography>
+        </IconButton>
       </NavLink>
     </Box>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,31 @@
-const styles = {
-  container: {
-    minHeight: 'calc(100vh - 50px)',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontWeight: 500,
-    fontSize: 48,
-    textAlign: 'center',
-  },
-};
+import { Button, Typography } from '@mui/material';
+import { Box } from '@mui/system';
+import { NavLink } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <div style={styles.container}>
-      <h1 style={styles.title}>
-        Phonebook manager welcome page{' '}
-        <span role="img" aria-label="Greeting icon">
-          💁‍♀️
-        </span>
-      </h1>
-    </div>
+    <Box>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          textAlign: 'center',
+          mt: '75px',
+          mb: '35px',
+        }}
+      >
+        <Typography variant="h3" component="h1">
+          Phonebook manager welcome page
+        </Typography>
+      </Box>
+      <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+        <NavLink style={{ textDecoration: 'none' }} to="/login">
+          <Button size="large" variant="contained">
+            Get started
+          </Button>
+        </NavLink>
+      </Box>
+    </Box>
   );
 };
 
